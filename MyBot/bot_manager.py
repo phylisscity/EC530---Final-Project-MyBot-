bb0f3d3c154e634137fead89c51a4ce0e8dea722
Phyliss Darko <phylissd@bu.edu>
2025-04-27 20:20:36 -0400

    updated botmanager to handle grid sizes

--- MyBot/bot_manager.py
+++ MyBot/bot_manager.py
@@ -201,10 +201,10 @@ class BotManager:
     Handles create, move, and status requests.
     """
 
-    def __init__(self):
+    def __init__(self, width=0, height=0, num_stations=5):
         # Store all bots in a dictionary with their ID as the key
         self.bots = {}
-        self.grid = Grid()  # grid object!
+        self.grid = Grid(width, height, num_stations)  # grid object!
         self.shared_goal = None  # Track the competitive goal (x, y)
         self.shared_goal_claimed = False  # Track if goal is captured
         self.grid.manager = self  #tells the grid who owns it
